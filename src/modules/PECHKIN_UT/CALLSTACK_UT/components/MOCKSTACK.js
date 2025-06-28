1cd07bb107456205ab61bd2566874fabee543763
Max Matt <77360455+Vsemhoy@users.noreply.github.com>
2025-06-28 18:58:50 +0300

    up

--- src/modules/PECHKIN_UT/CALLSTACK_UT/components/MOCKSTACK.js
+++ src/modules/PECHKIN_UT/CALLSTACK_UT/components/MOCKSTACK.js
@@ -1,7 +1,8 @@
+import { CiCircleOutlined, CiOutlined, DollarCircleOutlined } from "@ant-design/icons";
 
 
 export const CALL_DATA = {
-    id: 0,
+    id: 534,
     name: 'Hello',
     link: 'http://hello.world',
     method: 2,
@@ -9,8 +10,15 @@ export const CALL_DATA = {
     tags: "",
     is_cron: true,
     starred: false,
-    request_body: '',
-    sort_order:  1
+    request_body: {},
+    sort_order:  1,
+    cron_interval: 0,
+    cron_minutes: "",
+    auto_run: false,
+    user_id: 46,
+    created_at: null,
+    updated_at: null,
+    group_id: 0,
 }
 
 export const CALL_DEFAULT_POST = {
@@ -19,3 +27,37 @@ export const CALL_DEFAULT_POST = {
     },
     _token: "{{TOKEN}}"
 };
+
+
+export const GROUPS_CALL_ITEMS = [
+    {
+        id: 0,
+        name: "Empty",
+        icon: <CiCircleOutlined />,
+        color: "#444444",
+    },
+    {
+        id: 1,
+        name: "SALEC",
+        icon: <DollarCircleOutlined />,
+        color: "#4488bb",
+    },
+    {
+        id: 2,
+        name: "SKUD",
+        icon: <DollarCircleOutlined />,
+        color: "#44bb88",
+    },
+    {
+        id: 3,
+        name: "MAIN",
+        icon: <DollarCircleOutlined />,
+        color: "#4400bb",
+    },
+        {
+        id: 4,
+        name: "SERTIFF",
+        icon: <DollarCircleOutlined />,
+        color: "#bb8833",
+    },
+]
--- src/modules/PECHKIN_UT/CALL_PAGE_UT/components/style/CallPageUt.jsx
+++ src/modules/PECHKIN_UT/CALL_PAGE_UT/components/style/CallPageUt.jsx
@@ -1,11 +1,12 @@
 import { FileFilled, PlayCircleOutlined } from '@ant-design/icons';
-import { Button, Input, Radio, Select } from 'antd';
+import { Button, Checkbox, Input, Radio, Select, Switch } from 'antd';
 import TextArea from 'antd/es/input/TextArea';
 import { githubDarkTheme, JsonEditor } from 'json-edit-react';
 import React, { useEffect, useState } from 'react';
-import { CALL_DEFAULT_POST } from '../../../CALLSTACK_UT/components/MOCKSTACK';
+import { CALL_DATA, CALL_DEFAULT_POST, GROUPS_CALL_ITEMS } from '../../../CALLSTACK_UT/components/MOCKSTACK';
 import { PROD_AXIOS_INSTANCE } from '../../../../../API/API';
-import { CSRF_TOKEN, HTTP_HOST } from '../../../../../config/config';
+import { CSRF_TOKEN, HTTP_HOST, PRODMODE } from '../../../../../config/config';
+import { useParams } from 'react-router-dom';
 
 
 const OptimizedInput = React.memo(({ value, onChange, placeholder }) => {
@@ -25,9 +26,11 @@ const OptimizedInput = React.memo(({ value, onChange, placeholder }) => {
 
 
 const CallPageUt = (props) => {
+
+  const {call_id} = useParams();
   const [activeTab, setActiveTab] = useState('body');
 
-    const [itemId, setItemId] = useState(425);
+    const [itemId, setItemId] = useState(call_id);
     const [method, setMethod] = useState(1);
     const [name, setName] = useState('initialValue');
     const [link, setLink] = useState('http://192.168.1.16/hello');
@@ -38,6 +41,35 @@ const CallPageUt = (props) => {
     const [sortOrder, setSortOrder] = useState(1);
 
     const [autoCall, setAutoCall] = useState(false);
+    const [cronInterval, setCronInterval] = useState(0);
+    const [cronMinutes, setCronMinutes] = useState("");
+    const [autoRun, setAutoRun] = useState(false);
+    const [userId, setUserId] = useState(null);
+    const [groupId, setGroupId] = useState(0);
+
+    const [baseData, setBaseData] = useState(null);
+    
+  useEffect(() => {
+    if (PRODMODE){
+
+    } else {
+      setBaseData(CALL_DATA);
+    }
+  }, [itemId]);
+
+
+  useEffect(() => {
+    console.log('baseData', baseData)
+    if (baseData){
+      setMethod(baseData?.method);
+      setName(baseData.name);
+      setLink(baseData.link);
+      setDescription(baseData.description);
+      setTags(baseData.tags);
+      setRequestBody(baseData.request_body);
+    }
+  }, [baseData]);
+
 
   const [requestBody, setRequestBody] = useState( JSON.parse(JSON.stringify(CALL_DEFAULT_POST)));
 
@@ -94,22 +126,26 @@ const CallPageUt = (props) => {
                 style={{width: '120px'}}
                   placeholder="Select a person"
                   optionFilterProp="children"
-                  // onChange={onChange}
+                  value={method}
+                  onChange={setMethod}
                   // onSearch={onSearch}
                   filterOption={(input, option) =>
                     (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
                   }
                   options={[
                     {
-                      value: '1',
+                      key: 'dfklajd',
+                      value: 1,
                       label: 'GET',
                     },
                     {
-                      value: '2',
+                      key: 'dfjaskd',
+                      value: 2,
                       label: 'POST',
                     },
                     {
-                      value: '3',
+                      key: 'dfakjd',
+                      value: 3,
                       label: 'PUT',
                     },
                   ]}
@@ -119,16 +155,19 @@ const CallPageUt = (props) => {
             <Input 
             size={'large'}
              style={{width: '100%'}}
-             value={"hello"}
+             value={name}
              placeholder={'Name'}
+             maxLength={120}
+                  onChange={(ev)=>{setName(ev.target.value)}}
              />
           </div>
            <div style={{width: '100%'}} >
             <Input 
             size={'large'}
              style={{width: '100%'}}
-             value={"hello"}
+             value={link}
              placeholder={'URL'}
+             onChange={(ev)=>{setLink(ev.target.value)}}
              />
           </div>
           
@@ -146,8 +185,9 @@ const CallPageUt = (props) => {
            <div style={{width: '100%'}} >
             <TextArea 
              style={{width: '100%'}}
-             value={""}
+             value={description}
              placeholder={'Description'}
+             onChange={(ev)=>{setDescription(ev.target.value)}}
              />
           </div>
         </div>
@@ -161,23 +201,99 @@ const CallPageUt = (props) => {
                 <Radio.Button value="response">Response</Radio.Button>
               </Radio.Group>
           </div>
+          <div>{call_id}</div>
         </div>
 
         {activeTab == 'body' ? (
-          <div className={'ut-table-list-stack ut-pa-12'} style={{ width: '100%' }}>
-
-
+          <div className={'ut-table-list-stack ut-pa-12 ut-col-3-1'} style={{ width: '100%' }}>
+            <div>
             <JsonEditor
               style={{ width: '100%', minHeight: '300px' }}
               data={requestBody}                // <-- правильно: data, не date
               setData={setRequestBody}          // <-- функция обновления данных
               theme={githubDarkTheme}            // если используешь тему
             />
+            </div>
+            <div className={'ut-side-panel'}>
+                          <Checkbox 
+                checked={isCron}
+                onChange={(ev)=>{setIsCron(ev.target.checked)}}
+               > Авто триггер</Checkbox>
+              <br />
+              <br />
+            {isCron == true && (
+              <>
+              <Checkbox 
+                  checked={autoRun}
+                onChange={(ev)=>{setAutoRun(ev.target.checked)}}
+              >  Запускать при входе в утиль</Checkbox>
+              <br />
+              <br />
+              <label>Интервал запуска в минутах</label>
+              <br /> 
+              <br /> 
+              <Input
+                value={cronInterval}
+                onChange={(ev)=>{setCronInterval(ev.target.value)}}
+                placeholder='интервал в минутах'
+               type={'number'} min={'1'} ></Input>
+              <br />
+              <br />
+              <label>Или запускать каждый час в N минут</label>
+              <br /> 
+              <br /> 
+                <Input
+                  value={cronMinutes}
+                  onChange={(ev)=>{setCronMinutes(ev.target.value)}}
+                  placeholder='запускать в указанные минуты часа'
+                  type={'text'} maxLength={100}></Input>
+                <br /> 
+            </>
+            )}
+
+              
+              <br /> 
+                <label>А как насчёт тегов?</label>
+              <br /> 
+              <br /> 
+                <Input
+                                value={tags}
+                onChange={(ev)=>{setTags(ev.target.value)}}
+                  placeholder='просто текст'
+                type={'text'} maxLength={64}></Input>
+              <br /> 
+              <br /> 
+              <label>Позиция в куче</label>
+              <br /> 
+              <br /> 
+                <Input
+                                value={sortOrder}
+                onChange={(ev)=>{setSortOrder(ev.target.value)}}
+                  type={'number'} min={'1'} max={'32000000'}
+                  placeholder='Больше-выше'
+                  ></Input>
+              <br /> 
+              <br /> 
+              <label>Группа</label>
+              <br /> 
+              <br /> 
+                <Select 
+                  value={groupId}
+                  onChange={(ev)=>{setGroupId(ev)}}
+                  style={{width: '100%'}}
+                  options={GROUPS_CALL_ITEMS.map((item)=>(
+                    {key: item.id,
+                    label: item.name,
+                    value: item.id
+                  }))}
+                />
+                  
+            </div>
             
           </div>
         ) : (
           <div className={'ut-table-list-stack ut-pa-12'} style={{ width: '100%' }}>
-            Helfalsdfkjas lkdjfkl
+            Сюда мы поместим респонс ответ от сервера
           </div>
         )}
 
